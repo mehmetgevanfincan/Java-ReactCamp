2874c717a2c513ad08904c17757b212a4a9e25c9
mehmetgevanfincan <m.gevanfincan@gmail.com>
2022-10-04 17:13:30 +0300

    Inheritance

---
+++ JavaOdev2/javaOdev2-2/Inheritance/PersonManager.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class PersonManager {
+    public void List() {
+        System.out.println("Müşteri Listelendi");
+    }
+    public void Add() {
+        System.out.println("Müşteri Eklendi");
+    }
+}
