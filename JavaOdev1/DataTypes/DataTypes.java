189c2119d943ccf78ac236e149a6ed6608d822bb
mehmetgevanfincan <m.gevanfincan@gmail.com>
2022-10-04 15:29:53 +0300

    Day 1 Homework 3

---
+++ JavaOdev1/DataTypes/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+	     //Ondalıklı sayı tipleri ==> float, double
+	     double sayi1 = 12.5;
+	     sayi1 = -129;
+
+	     //char: karakterlerin tutulduğu tip
+	     char karakter = 'x';
+
+	     //boolean: true/false değerlerini tutar.
+	     boolean dogruMu = true;
+
+	}
+
+}
